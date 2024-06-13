4cb4c81a66608b9da606ca42a94d88069a65c88e
ProphesierC <bwknowles98@gmail.com>
2024-06-13 01:47:36 -0400

    datetime logic for values needed

--- scripts/index.js
+++ scripts/index.js
@@ -2,70 +2,89 @@ class Calendar
 {
     constructor() 
     {
-        this.inputArray = Array.from(document.querySelectorAll('input[type=number]')); //iterable array from nodelists
+        this.inputArray = Array.from(document.querySelectorAll('input[type=number]'));
         this.toggleArray = Array.from(document.querySelectorAll('[class^="dropdown"]'));
 
-        document.getElementById("day-dropdown").classList.add("hidden");        
+        document.getElementById("day-dropdown").classList.add("hidden");
         document.getElementById("month-dropdown").classList.add("hidden");
         document.getElementById("year-dropdown").classList.add("hidden");
-
-
-
         this.submitButton = document.querySelector('button');
-        
 
-        this.init(); 
+
+        this.outputYears = document.getElementById("output-years");
+        this.outputMonths = document.getElementById("output-months");
+        this.outputDays = document.getElementById("output-days");        
+
+        this.dateTime();
+        this.init();
     }
 
-    dropdown() 
+    dateTime() 
     {
-        
+        this.date = new Date();
+        this.year = this.date.getFullYear();
+        this.month = this.date.getMonth() + 1;
+        this.now = new Date(this.year, this.month, 0);
+        this.daysThisMonth = this.now.getDate();
+    }
+
+    output()
+    {
+        this.outputYears.textContent = `--years`; //math wip
+        this.outputMonths.textContent = `--months`
+        this.outputDays.textContent = `--days`
+    }
+
+    dropdown(id, start, end) 
+    {
+        let dropdownValue = document.getElementById(id);
+
+        for (let i = start; i <= end; i++) 
+        {            
+            let option = document.createElement("option");
+            option.value = i;
+            option.textContent = i;
+            dropdownValue.appendChild(option);
+
+            option.addEventListener('click', () => 
+            {
+                let inputId = id.replace('dropdown', 'button-input');
+                document.getElementById(inputId).value = i;
+                dropdownValue.classList.add("hidden");
+            });
+        }
+
         this.inputArray.forEach((input, index) => 
         {
-            let toggleItem = this.toggleArray[index].classList;            
+            let toggleItem = this.toggleArray[index].classList;
 
             input.onclick = () => 
             {
-                toggleItem.toggle("hidden");        
+                toggleItem.toggle("hidden");
             }
-            input.onblur = () => 
-            {
-                toggleItem.contains("hidden") ? null : toggleItem.add("hidden");
-            }  
 
-        });        
+            input.onblur = async () =>
+            {                
+                await new Promise(resolve => setTimeout(resolve, 10));
+                toggleItem.contains("hidden") ? null : toggleItem.add("hidden");                
+            }
+        });
     }
 
-    dropdownValue()
-    {
-        this.selectMenu = document.getElementById("day-dropdown");
-        // this.selectMenu = document.getElementById("month-dropdown");
-        // this.selectMenu = document.getElementById("year-dropdown");
-
-        for (let i = 1; i <= 31; i++) 
-        {  
-            let tempOption = document.createElement("option");
-            let tempText = document.createTextNode(i);  
-            tempOption.appendChild(tempText);  
-            tempOption.setAttribute("value", i);  
-            this.selectMenu.appendChild(tempOption); 
-        }
-
-    }
-
-    submit()
+    submit() 
     {
         this.submitButton.addEventListener('click', () => 
         {
-
-        })
+            
+        });
     }
-
 
     init() 
     {
-        this.dropdown();
-        this.dropdownValue();
+        this.dropdown('day-dropdown', 1, this.daysThisMonth);
+        this.dropdown('month-dropdown', 1, 12);
+        this.dropdown('year-dropdown', this.year - 50, this.year + 50);
+        this.output()
         this.submit();
     }
 }
