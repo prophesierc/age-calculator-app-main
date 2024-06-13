ca2fb1de70114a8e48d72051d0e160e3d050ad39
ProphesierC <bwknowles98@gmail.com>
2024-06-12 22:54:02 -0400

    main dropdown and onblur event work

--- scripts/index.js
+++ scripts/index.js
@@ -2,36 +2,72 @@ class Calendar
 {
     constructor() 
     {
-        
+        this.inputArray = Array.from(document.querySelectorAll('input[type=number]')); //iterable array from nodelists
+        this.toggleArray = Array.from(document.querySelectorAll('[class^="dropdown"]'));
 
-        this.toggleList = document.querySelectorAll('[class^="dropdown"]');
-        this.inputList = document.querySelectorAll('input[type=number]');       
+        document.getElementById("day-dropdown").classList.add("hidden");        
+        document.getElementById("month-dropdown").classList.add("hidden");
+        document.getElementById("year-dropdown").classList.add("hidden");
+
+
+
+        this.submitButton = document.querySelector('button');
+        
 
         this.init(); 
     }
 
-    
-    dropdown()
+    dropdown() 
     {
-        this.inputList[0].addEventListener('click', () =>
+        
+        this.inputArray.forEach((input, index) => 
         {
-            document.getElementById("day-dropdown").classList.toggle("day-toggle");
-        });
-        this.inputList[1].addEventListener('click', () =>
-        {
-            document.getElementById("month-dropdown").classList.toggle("month-toggle");
-        });
-        this.inputList[2].addEventListener('click', () =>
-        {
-            document.getElementById("year-dropdown").classList.toggle("year-toggle");
-        });
+            let toggleItem = this.toggleArray[index].classList;            
+
+            input.onclick = () => 
+            {
+                toggleItem.toggle("hidden");        
+            }
+            input.onblur = () => 
+            {
+                toggleItem.contains("hidden") ? null : toggleItem.add("hidden");
+            }  
+
+        });        
     }
 
-    init()
+    dropdownValue()
     {
-        this.dropdown()
+        this.selectMenu = document.getElementById("day-dropdown");
+        // this.selectMenu = document.getElementById("month-dropdown");
+        // this.selectMenu = document.getElementById("year-dropdown");
+
+        for (let i = 1; i <= 31; i++) 
+        {  
+            let tempOption = document.createElement("option");
+            let tempText = document.createTextNode(i);  
+            tempOption.appendChild(tempText);  
+            tempOption.setAttribute("value", i);  
+            this.selectMenu.appendChild(tempOption); 
+        }
+
     }
-        
+
+    submit()
+    {
+        this.submitButton.addEventListener('click', () => 
+        {
+
+        })
+    }
+
+
+    init() 
+    {
+        this.dropdown();
+        this.dropdownValue();
+        this.submit();
+    }
 }
 
 let calendar = new Calendar();
